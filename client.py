aba30624de79c9bb0f30cb5c8b3748d49b39ea6b
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-08-18 16:32:48 -0500

    client modifications

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
 
+# TCP    127.0.0.1:51125        127.0.0.1:65432        TIME_WAIT
+
 HOST = '127.0.0.1'
 PORT = 65432
 
+print(f"Using Host:{HOST} at Port:{PORT}\n")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello World')
+    # send() -> needs to be called as many times data need to be sent, so use sendall()
+    s.sendall(b'Hello World')   # seding byte-like object and not string
     data = s.recv(1024)     # read server reply and print
 
-print('Received', repr(data))
+print('Data received: ', repr(data))
--- single_server.py
+++ single_server.py
@@ -1,5 +1,7 @@
 import socket
 
+# TCP    127.0.0.1:51125        127.0.0.1:65432        TIME_WAIT
+
 HOST = "127.0.0.1"  # if empty, server accepts connections on all IPv4 interfaces
 PORT = 65432        # PORT -> 1 - 65535
 
@@ -8,12 +10,15 @@ PORT = 65432        # PORT -> 1 - 65535
 # IPv4 address family -> AF_INET
 # IPv6 address family -> AF_INET6
 
+print("Waiting for Connection Request \n")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # socket.socket -> using socket without socket close()
     s.bind((HOST, PORT))    # HOST -> Network interface, PORT -> PORT
     # IPv4 -> (host, port), IPv6 -> (host, port, flowinfo, scopeid)
     s.listen()  # default backlog value is used - number of connections to accept
     conn, addr = s.accept()
+    # print(f'Connection established with: {conn}, {addr}')
     with conn:
         print('Connected using', addr)
         while True:
